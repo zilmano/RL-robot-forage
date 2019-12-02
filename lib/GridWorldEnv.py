ca48c487cf5dc98fab1ce1fb7a122f438a4bb17a
zilmano <zilmano@gmail.com>
2019-12-01 21:16:54 -0600

    Add initialization of graph. Calc item probabilities for each state (grid cell)

--- lib/GridWorldEnv.py
+++ lib/GridWorldEnv.py
@@ -3,30 +3,40 @@ import string
 from enum import IntEnum
 import numpy as np
 import utility as util
+from scipy import stats
+import math
+from graph import GridGraphWithItems
+import sys
+
 
 class Actions(IntEnum):
-    west  = 0
+    west = 0
     north = 1
-    east  = 2
+    east = 2
     south = 3
 
+
 class Item:
-    def __init__(self,mean,variance,name,m,n):
+    def __init__(self, mean, variance, name, m, n):
         self.mean = mean
         self.variance = variance
         self.state = None
         self.name = name
         self.m = m
         self.n = n
+        mean_y, mean_x = self.getRowAndColFromState()
+        self.mean_y = m - 0.5 - mean_y
+        self.mean_x = mean_x+ 0.5
 
-    def getRowAndCol(self):
-        return ((self.mean % m), (item.mean / m))
+    def getRowAndColFromState(self):
+        return (self.mean % self.m), (int(self.mean/self.m))
 
-    def setStateFromRowAndCol(self,row,col):
+    def setStateFromRowAndCol(self, row, col):
         self.state = int(col*self.m + row)
 
+
 class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
-    def __init__(self,m,n,k,debug=False):
+    def __init__(self, m, n, k, debug=False):
         # 20x20 grid world
         STATE_SPACE_SIZE = m*n
         self.m = m
@@ -39,10 +49,13 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         self._final_state = False
         self._state = None
         self.trans_mat = None
-        self.rmat = None
+        self.r_mat = None
+        self.item_loc_probabilities = None
+        self.items_to_go = None
+        self.already_visited = None
         # nS states of mxn grid. State numbering is by columns starting from top-left. i.e:
         # top-row, leftmost-col state = 0, bottom-leftmost state = m-1, top-row,second from left col = m
-        # 2nd from top row, 2nd from left colunm = m+1.  etc...
+        # 2nd from top row, 2nd from left column = m+1.  etc...
 
         # Actions:
         # 0 -west, 1 -north, 2-east, 3 -south
@@ -56,29 +69,77 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         self._Q = np.zeros([self.spec.nS, self.spec.nA])
 
         self._generateItems()
+        self._initItemsProbabilities()
+        #for i in range(0, k):
+        #    print(self.item_loc_probabilities[i])
+        #    print(self.item_loc_probabilities[i].sum())
+        #sys.exit()
+        self.graph_rep = GridGraphWithItems(m, n, k, self.item_loc_probabilities)
         self.reset()
         '''for i in range(0,self.spec.nS):
             util.logmsg("state:{}\n   {}".format(i,self.r_mat[i]))'''
 
-
     def _generateItems(self):
-        for i in range(0,self.k):
+        for i in range(0, self.k):
             mean = np.random.randint(low=0, high=self.spec.nS, size=1)[0]
-            variance = np.random.randint(low=2,high=5,size=1)[0]
-            self.items.append(Item(mean,variance,string.ascii_lowercase[i],self.m,self.n))
+            variance = np.random.randint(low=2, high=5, size=1)[0]
+            self.items.append(Item(mean, variance, string.ascii_lowercase[i], self.m, self.n))
+
+    def _initItemsProbabilities(self):
+        # Calc items probabilities to be in states
+        self.item_loc_probabilities = np.zeros([self.k, self.spec.nS])
+        for i, item in enumerate(self.items):
+            mv_gaussian = stats.multivariate_normal(mean=[item.mean_x, item.mean_y], cov=[[item.variance, 0],
+                                                                                      [0, item.variance]])
+            '''' 
+            grid coding as follows (example 4x4), state num is inside each grid cell:
+            Y  
+            4 _________________
+            3 _0_|_4_|_8__|_12_|
+            2 _1_|_5_|_9__|_13_|
+            1 _2_|_6_|_10_|_14_|
+            0 _3_|_7_|_11_|_15_|
+                 1   2    3    4  X
+
+            cells on the edge of the grid have bigger probabilities then the cells in the middle, as they get all 
+            probabilities of the space beyond them. 
+            For instance, the probability of the item x being from -inf to 1 will be crammed into cells rightmost col 
+            (states 0,1,2,3). The probability of the item being from 3 to inf will be crammed into cell on the leftmost 
+            col (states 12,13,14,15). 
+            etc..
+            For example, the probability of the item being in cell 2 is the probability of item's x being -inf<x<=1,
+            and y being 1<y<=2            
+            '''
+
+            for bottom_cell in range(self.m - 1, self.spec.nS, self.m):
+                sum_of_prev_col_cell_probs = 0
+                for row in reversed(range(0, self.m)):
+                    state = bottom_cell - (self.m - 1) + row
+                    (row, col) = self._getRowColFromState(state)
+                    cell_top_left_y, cell_top_left_x = (self.m - row, col+1)
+                    if not state % self.m:
+                        cell_top_left_y = math.inf
+                    if col == (self.n - 1):
+                        cell_top_left_x = math.inf
+                    state_prob = mv_gaussian.cdf([cell_top_left_x, cell_top_left_y])
+                    state_prob -= sum_of_prev_col_cell_probs
+                    if col >= 1:
+                        state_prob -= mv_gaussian.cdf([col, cell_top_left_y])
+                    self.item_loc_probabilities[i][state] = state_prob
+                    sum_of_prev_col_cell_probs += state_prob
 
     def _placeItems(self):
-        self.item_locations =  [[[] for i in range(0, self.n)] for i in range(0, self.m)]
+        self.item_locations = [[[] for i in range(0, self.n)] for j in range(0, self.m)]
         for item in self.items:
-            (mean_x,mean_y) = self._getRowColFromState(item.mean)
-            ((x,y),) = np.random.multivariate_normal([mean_x,mean_y],[[item.variance,0],[0,item.variance]],1)
+            ((x, y),) = np.random.multivariate_normal([item.mean_x, item.mean_y], [[item.variance, 0], [0,
+                                                                                                   item.variance]], 1)
             x = self.m - 1 if x > self.m - 1 else (0 if x < 0 else x)
             y = self.n - 1 if y > self.n - 1 else (0 if y < 0 else y)
-            item.setStateFromRowAndCol(np.rint(x),np.rint(y))
+            item.setStateFromRowAndCol(np.rint(x), np.rint(y))
             self.item_locations[int(np.rint(x))][int(np.rint(y))].append(item)
 
-    def _getRowColFromState(self,state):
-        return ((state % self. m), int(state / self.m))
+    def _getRowColFromState(self, state):
+        return (state % self. m), int(state / self.m)
 
     def _getStateFromColRow(self, col, row):
         return col*self.m + row
@@ -88,42 +149,42 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         r_mat = np.zeros((self.spec.nS, len(Actions), self.spec.nS))
 
         # Build transition matrix
-        for state in range(0,self.spec.nS):
+        for state in range(0, self.spec.nS):
 
-                if state < self.m:
-                    trans_mat[state][Actions.west][state] = 1
-                    r_mat[state][Actions.west][state] = -10
-                else:
-                    trans_mat[state][Actions.west][state - self.m] = 1
-                    r_mat[state][Actions.west][state - self.m] = -1
+            if state < self.m:
+                trans_mat[state][Actions.west][state] = 1
+                r_mat[state][Actions.west][state] = -10
+            else:
+                trans_mat[state][Actions.west][state - self.m] = 1
+                r_mat[state][Actions.west][state - self.m] = -1
 
-                if state in range(0,self.spec.nS,self.m):
-                    trans_mat[state][Actions.north][state] = 1
-                    r_mat[state][Actions.north][state] = -10
-                else:
-                    trans_mat[state][Actions.north][state - 1] = 1
-                    r_mat[state][Actions.north][state - 1] = -1
+            if state in range(0, self.spec.nS, self.m):
+                trans_mat[state][Actions.north][state] = 1
+                r_mat[state][Actions.north][state] = -10
+            else:
+                trans_mat[state][Actions.north][state - 1] = 1
+                r_mat[state][Actions.north][state - 1] = -1
 
-                if state >= ((self.n)-1) * self.m:
-                    trans_mat[state][Actions.east][state] = 1
-                    r_mat[state][Actions.east][state] = -10.
-                else:
-                    trans_mat[state][Actions.east][state + self.m] = 1
-                    r_mat[state][Actions.east][state + self.m] = -1
+            if state >= (self.n-1) * self.m:
+                trans_mat[state][Actions.east][state] = 1
+                r_mat[state][Actions.east][state] = -10.
+            else:
+                trans_mat[state][Actions.east][state + self.m] = 1
+                r_mat[state][Actions.east][state + self.m] = -1
 
-                if state in range(self.m-1, self.spec.nS, self.m):
-                    trans_mat[state][Actions.south][state] = 1
-                    r_mat[state][Actions.south][state] = -10.
-                else:
-                    trans_mat[state][Actions.south][state + 1] = 1
-                    r_mat[state][Actions.south][state + 1] = -1
+            if state in range(self.m-1, self.spec.nS, self.m):
+                trans_mat[state][Actions.south][state] = 1
+                r_mat[state][Actions.south][state] = -10.
+            else:
+                trans_mat[state][Actions.south][state + 1] = 1
+                r_mat[state][Actions.south][state + 1] = -1
 
         for item in self.items:
-            if (item.state < ((self.n)-1) * self.m):
+            if item.state < (self.n-1) * self.m:
                 r_mat[item.state+self.m][Actions.west][item.state] += 20
-            if  item.state >= self.m:
+            if item.state >= self.m:
                 r_mat[item.state-self.m][Actions.east][item.state] += 20
-            if  item.state not in range(0,self.spec.nS,self.m):
+            if item.state not in range(0, self.spec.nS, self.m):
                 r_mat[item.state - 1][Actions.south][item.state] += 20
             if item.state not in range(self.m - 1, self.spec.nS, self.m):
                 r_mat[item.state+1][Actions.north][item.state] += 20
@@ -131,7 +192,7 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         return trans_mat, r_mat
 
     def _update_trans_mat(self):
-        if (self._state < ((self.n) - 1) * self.m):
+        if self._state < (self.n - 1) * self.m:
             self.r_mat[self._state + self.m][Actions.west][self._state] = -5
         if self._state >= self.m:
             self.r_mat[self._state - self.m][Actions.east][self._state] = -5
@@ -140,12 +201,11 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         if self._state not in range(self.m - 1, self.spec.nS, self.m):
             self.r_mat[self._state + 1][Actions.north][self._state] = -5
 
-
     def _items_found(self, row, col):
         self.items_to_go -= len(self.item_locations[row][col])
         self.item_locations[row][col] = []
 
-    def reset(self,random_state = False):
+    def reset(self, random_state=False):
         if not random_state:
             self._state = 0
         else:
@@ -161,7 +221,6 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
             util.logmsg("## Generated Items: ")
             for item in self.items:
                 util.printobj(item)
-
 
 
         (row, col) = self._getRowColFromState(self._state)
@@ -188,8 +247,8 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         return self._state
 
     def step(self, action):
-        assert action >=Actions.west and action < self.spec.nA, "Invalid Action!"
-        assert self._state >= 0 and self._state < self.spec.nS , "Invalid State!"
+        assert Actions.west <= action < self.spec.nA, "Invalid Action!"
+        assert 0 <= self._state < self.spec.nS, "Invalid State!"
         assert not self._final_state, "Episode has already finished. Please restart!"
 
         prev_state = self._state
@@ -200,11 +259,11 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
             util.logmsg("taking step with a:{}...".format(action))
             util.logmsg("s:{} r:{}".format(self._state, r))
 
-        (row,col) = self._getRowColFromState(self._state)
+        (row, col) = self._getRowColFromState(self._state)
         if len(self.item_locations[row][col]) > 0:
             if self.debug:
                 util.logmsg("items found!")
-            self._items_found(row,col)
+            self._items_found(row, col)
 
         self._update_trans_mat()
         self.already_visited[self._state] = 1
@@ -214,7 +273,7 @@ class GridWorld(Env):  # MDP introduced at Fig 5.4 in Sutton Book
         else:
             return self._state, r, False
 
-    def step_using_policy(self,pi):
+    def step_using_policy(self, pi):
         action = pi.action(self._state)
         return self.step(action) + (action,)
 
